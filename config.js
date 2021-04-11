d55fbcf030094ab669660e6532944b54bd466b89
VISION-DARK <67938081+VISION-DARK@users.noreply.github.com>
2021-04-12 05:44:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.packname = 'FAKHRUL'
+global.author = 'BOT'
 
 global.multiplier = 69 // The higher, The harder levelup
 

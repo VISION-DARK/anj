a74a4262b23581e231b11808c234e3d99d6d7180
VISION-DARK <67938081+VISION-DARK@users.noreply.github.com>
2021-04-11 07:48:56 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
+  // this.sendContact(m.chat, '62895618376962', 'Fgans ✔️', m)
+  this.sendContact(m.chat, '62895618376962', 'Fgans ✔️', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
